00db6248c90325153c422d35bf708bd2ecc7e69d
nebula-2020 <2781536086@qq.com>
2021-02-03 19:18:13 +0800

    chore:新增文章相关repository
    新增2个repository

---
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -0,0 +1,26 @@
+/*
+ * 文件名：ArticleRepository.java
+ * 描述：单表存储库
+ * 修改人：刘可
+ * 修改时间：2021-02-03
+ */
+package com.example.demo.repository;
+
+import java.math.BigInteger;
+
+import com.example.demo.entity.*;
+
+import org.springframework.data.jpa.repository.*;
+
+/**
+ * 文章信息存储库。
+ * 
+ * @author 刘可
+ * @version 1.0.0.0
+ * @since 2021-02-03
+ */
+public interface ArticleRepository
+        extends JpaRepository<Article, BigInteger>,
+        JpaSpecificationExecutor<Article>
+{
+}
